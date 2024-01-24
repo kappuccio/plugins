05f9ba54d39c85f9f459a05d4ab999623b1769d4
vlaufoo <kla.itanod@gmail.com>
2024-01-24 19:58:30 +0100

    SONGS

--- Yoshify-DiscJockey/Custom Songs/resample.sh
+++ Yoshify-DiscJockey/Custom Songs/resample.sh
@@ -1,9 +1,12 @@
 
 for file in *.mp3; do
-    ffprobe -v error -select_streams a:0 -show_entries stream=sample_rate -of default=noprint_wrappers=1:nokey=1 "$file" | grep -q 48000 || ffmpeg -i "$file" -ar 48000 -c:a libmp3lame -q:a 2 "${file%.mp3}_resampled.mp3"
+    ffprobe -v error -select_streams a:0 -show_entries stream=sample_rate -of default=noprint_wrappers=1:nokey=1 "$file" | grep -q 48000 || ffmpeg -i "$file" -ar 48000 -c:a libmp3lame -q:a 2 "CCC_${file%.mp3}_resampled.mp3"
 done
 
-for file in *_resampled.mp3; do
-    original_file="${file%_resampled.mp3}.mp3"
+echo "CCC_*_resampled.mp3"
+
+for file in CCC_*_resampled.mp3; do
+    original_file="${file#"CCC_"}"
+    original_file="${original_file%_resampled.mp3}.mp3"
     rm "$original_file"
 done
